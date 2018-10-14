c1256fdafecf5c5f330453215bbb148b06f6560c
gkalil23 <gkalil23@gmail.com>
2018-10-14 00:00:38 -0400

    Update src/mbgj/assignment1/Main.java

--- src/mbgj/assignment1/Main.java
+++ src/mbgj/assignment1/Main.java
@@ -8,8 +8,20 @@ public class Main {
         
         //Board initializing
         BoardManager.init();                        
-        System.out.println("Board initialized");
-        
-        
+        System.out.println("Board initialized\n");
+      
+        for (int row = 0; row < 8; row++)
+        {
+          System.out.println("");
+          System.out.println("---------------------------------");
+
+          for (int column = 0; column < 8; column++)
+          {
+              System.out.print("| " + " " + " ");
+          }           
+        }
+        System.out.println("");
+        System.out.println("---------------------------------");
+    
     }
 }
